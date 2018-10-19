f67db392db156cc83f8a2fa61b8601993de5cf3b
Farid <relampagolanzadel04@gmail.com>
2018-10-20 00:12:29 +0530

    Initial commit

--- src/app.js
+++ src/app.js
@@ -1,14 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 
 import 'normalize.css/normalize.css';
 import './styles/style.scss';
 
-const template = (
-  <div>
-    Some sample text
-  </div>
-);
+import MainApp from './components/MainApp';
+
+const someReducer = (state = {}, action) => {
+  return state;
+};
+
+const store = createStore(someReducer);
 
 const appRoot = document.querySelector('#root');
-ReactDOM.render(template, appRoot);
+ReactDOM.render(
+  <Provider store={store}>
+    <MainApp />
+  </Provider>,
+  appRoot
+);
---
+++ src/components/MainApp.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class MainApp extends Component {
+  render() {
+    return (
+      <header className='header__heading'>
+        <h1>This is the header</h1>
+        <h3>This is the sub heading</h3>
+      </header>
+    );
+  }
+}
+
+export default MainApp;
